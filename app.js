33b05c3bb4b4ede2c59361c59e7c9ec448487dfb
Kananek Thongkam <info.dvgamer@gmail.com>
2017-11-24 16:46:05 +0700

    update client to touno.co engine

--- app.js
+++ app.js
@@ -1,39 +1,31 @@
-const express = require('express')
 const request = require('request-promise')
-var app = express()
+const moment = require('moment')
+const cron = require('cron')
 
+const ipAddrUpdate = async (domain) => {
+  console.time(`[couldfare.com] Updated Domain '${domain}'`)
+  let api = await request({
+    url: 'https://api.ipify.org?format=json',
+    json: true
+  })
+  await request({
+    method: 'POST',
+    url: 'https://touno.co/api/dns/update-ip',
+    fromData: {
+      'domain': domain,
+      'ipaddr': api.ip
+    }
+  })
+  console.timeEnd(`[couldfare.com] Updated Domain '${domain}'`)
+  console.log(`[couldfare.com] at ${moment().format('YYYY-MM-DD HH:mm:ss')} IP:'${api.ip}'`)
+}
 
-console.time('checkip')
-request({
-	method: 'GET',
-	url: 'https://api.ipify.org?format=json',
-	json: true
-}).then(res => {
-	console.timeEnd('checkip')
-	console.log('ipaddr', res)
-	return request.put({
-		url: 'https://api.cloudflare.com/client/v4/zones/b47ab1d676a910f9acfb3efe48938a59/dns_records/e0ecd8c8687432da092701fb15714569',
-		headers: {
-			'Content-Type':'application/json',
-			'X-Auth-Key':'541b7ac1734bfa838079b8908b31f49fa9262',
-			'X-Auth-Email':'info.dvgamer@gmail.com',
-		},
-		body: { "type":"A","name":"pc.touno.co","content": res.ip} ,
-		json: true,
-	})
-}).then(function(res){
-	console.log('cloudflare', res.result)
-}).catch(function(ex){
-	console.log('catch', ex.message)
+let addrUpdateTime = '30 * * * *'
+console.log(`[couldfare.com] corntab(${addrUpdateTime}) started.`)
+let addrUpdateJob = new cron.CronJob({
+  cronTime: addrUpdateTime,
+  onTick: ipAddrUpdate.bind(this, 'pc.touno.co'),
+  start: true,
+  timeZone: 'Asia/Bangkok'
 })
 
-// {"type":"A","name":"pc.touno.co","content": res.ip}
-// /Address:.([\d\.]+)/ig.exec(
-
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
