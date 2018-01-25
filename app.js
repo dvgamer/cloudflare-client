fb9a11994f5b31ff60ea11087e2922af34b2d6c0
Kananek T <info.dvgamer@gmail.com>
2018-01-26 02:56:33 +0700

    updated new project
    add eslint
    chagen url and domain to env
    add package script

--- app.js
+++ app.js
@@ -2,34 +2,46 @@ const request = require('request-promise')
 const moment = require('moment')
 const cron = require('cron')
 
-const ipAddrUpdate = async (domain) => {
-  console.time(`[couldfare.com] Updated Domain '${domain}'`)
+if (!process.env.DNS_UPDATE) throw new Error(`Required 'DNS_UPDATE' environment.`)
+if (!process.env.DOMAIN_NAME) throw new Error(`Required 'DOMAIN_NAME' environment.`)
+
+const toSeconds = hr => {
+  let seconds = (hr[0] + (hr[1] / 1e9)).toFixed(3)
+  return seconds
+}
+
+const ipAddrUpdate = async domain => {
+  let begin = process.hrtime()
   let api = await request({
     url: 'https://api.ipify.org?format=json',
     json: true
   })
-  await request({
+  let updated = await request({
     method: 'POST',
-    url: 'https://touno.co/api/dns/update-ip',
+    url: process.env.DNS_UPDATE,
     json: true,
-    body: {
-      domain: domain,
-      addr: api.ip
-    }
+    body: { domain: domain, addr: api.ip }
   })
-  console.timeEnd(`[couldfare.com] Updated Domain '${domain}'`)
-  console.log(`[couldfare.com] at ${moment().format('YYYY-MM-DD HH:mm:ss')} IP:'${api.ip}'`)
-  console.log(`[couldfare.com] -----------------------------------------`)
+  if (updated.err) {
+    console.log(`[couldfare.com] '${updated.err}' `)
+  } else {
+    console.log(`[couldfare.com] Updated '${domain}' at ${moment().format('YYYY-MM-DD HH:mm:ss')} IP:'${api.ip}' (${toSeconds(process.hrtime(begin))}s)`)
+  }
 }
 
-ipAddrUpdate('pc.touno.co')
+ipAddrUpdate(process.env.DOMAIN_NAME).catch(ex => {
+  console.log(`[couldfare.com] '${ex.message}' `)
+})
 
 let addrUpdateTime = '30 * * * *'
-console.log(`[couldfare.com] corntab(${addrUpdateTime}) started.`)
-let addrUpdateJob = new cron.CronJob({
+let jobUpdated = new cron.CronJob({
   cronTime: addrUpdateTime,
-  onTick: ipAddrUpdate.bind(this, 'pc.touno.co'),
+  onTick: () => {
+    ipAddrUpdate(this, process.env.DOMAIN_NAME).catch(ex => {
+      console.log(`[couldfare.com] '${ex.message}' `)
+    })
+  },
   start: true,
   timeZone: 'Asia/Bangkok'
 })
-
+console.log(`[couldfare.com] corntab(${addrUpdateTime}) ${jobUpdated.running ? 'started' : 'stoped'}.`)
