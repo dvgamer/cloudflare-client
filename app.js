a289f9d18b599072455b915a8d17a3889beb770e
Kananek T <info.dvgamer@gmail.com>
2018-03-21 23:38:31 +0700

    clouldface update only

--- app.js
+++ app.js
@@ -10,42 +10,33 @@ const ipAddrUpdate = async domain => {
     json: true
   })
   
-  if (process.env.CUSTOM_URL) {
-    let updated = await request({
-      method: 'POST',
-      url: process.env.CUSTOM_URL,
-      json: true,
-      body: { domain: domain, addr: api.ip, zone: process.env.DOMAIN_ZONE }
-    })
-    if (updated.err) throw new Error(`[couldfare.com] '${updated.err}' `)
-  } else {
-    if (!process.env.DOMAIN_KEY) throw new Error(`Required 'DOMAIN_KEY' environment.`)
-    if (!process.env.DOMAIN_EMAIL) throw new Error(`Required 'DOMAIN_EMAIL' environment.`)
+  if (!process.env.DOMAIN_KEY) throw new Error(`Required 'DOMAIN_KEY' environment.`)
+  if (!process.env.DOMAIN_EMAIL) throw new Error(`Required 'DOMAIN_EMAIL' environment.`)
 
-    const zone = process.env.DOMAIN_ZONE
-    const headers = {
-      'Content-Type': 'application/json',
-      'X-Auth-Key': process.env.DOMAIN_KEY,
-      'X-Auth-Email': process.env.DOMAIN_EMAIL
-    }
-    let getRecords = await request({
-      url: `https://api.cloudflare.com/client/v4/zones/${zone}/dns_records?name=${domain}`,
-      headers: headers,
-      json: true
-    })
-    if (!getRecords.success) throw new Error(`Not found record name '${domain}'`)
-    let putRecords = await request({
-      method: 'PUT',
-      url: `https://api.cloudflare.com/client/v4/zones/${zone}/dns_records/${getRecords.result[0].id}`,
-      headers: headers,
-      body: { type: getRecords.result[0].type, name: domain, content: api.ip },
-      json: true
-    })
-    if (!putRecords.success) throw new Error(`Can't PUT record name '${domain}'`)
+  const zone = process.env.DOMAIN_ZONE
+  const headers = {
+    'Content-Type': 'application/json',
+    'X-Auth-Key': process.env.DOMAIN_KEY,
+    'X-Auth-Email': process.env.DOMAIN_EMAIL
   }
+  let getRecords = await request({
+    url: `https://api.cloudflare.com/client/v4/zones/${zone}/dns_records?name=${domain}`,
+    headers: headers,
+    json: true
+  })
+  if (!getRecords.success) throw new Error(`Not found record name '${domain}'`)
+  let putRecords = await request({
+    method: 'PUT',
+    url: `https://api.cloudflare.com/client/v4/zones/${zone}/dns_records/${getRecords.result[0].id}`,
+    headers: headers,
+    body: { type: getRecords.result[0].type, name: domain, content: api.ip },
+    json: true
+  })
+  if (!putRecords.success) throw new Error(`Can't PUT record name '${domain}'`)
   console.log(`[couldfare.com] Updated '${domain}' at ${moment().format('YYYY-MM-DD HH:mm:ss')} IP:'${api.ip}' (${toSeconds(process.hrtime(begin))}s)`)
 }
 
+console.log(`[couldfare.com] Watch and updated '${process.env.DOMAIN_NAME}'.`)
 ipAddrUpdate(process.env.DOMAIN_NAME).catch(Raven)
 
 let addrUpdateTime = '30 * * * *'
