38259725940d7656e04c83b91ca19ae3f6bb2e45
Kananek T <info.dvgamer@gmail.com>
2018-03-22 00:40:06 +0700

    updated domain zone

--- index.js
+++ index.js
@@ -16,34 +16,34 @@ const ipAddrUpdate = async domain => {
   const zone = process.env.DOMAIN_ZONE
   const headers = {
     'Content-Type': 'application/json',
-    'X-Auth-Key': process.env.DOMAIN_KEY,
-    'X-Auth-Email': process.env.DOMAIN_EMAIL
+    'X-Auth-Key': process.env.DOMAIN_KEY.trim(),
+    'X-Auth-Email': process.env.DOMAIN_EMAIL.trim()
   }
   let getRecords = await request({
     url: `https://api.cloudflare.com/client/v4/zones/${zone}/dns_records?name=${domain}`,
     headers: headers,
+    timeout: 1000,
     json: true
   })
   if (!getRecords.success) throw new Error(`Not found record name '${domain}'`)
-  let putRecords = await request({
-    method: 'PUT',
-    url: `https://api.cloudflare.com/client/v4/zones/${zone}/dns_records/${getRecords.result[0].id}`,
-    headers: headers,
-    body: { type: getRecords.result[0].type, name: domain, content: api.ip },
-    json: true
-  })
-  if (!putRecords.success) throw new Error(`Can't PUT record name '${domain}'`)
-  console.log(`[couldfare.com] Updated '${domain}' at ${moment().format('YYYY-MM-DD HH:mm:ss')} IP:'${api.ip}' (${toSeconds(process.hrtime(begin))}s)`)
+  if (api.ip !== getRecords.result[0].content) {
+    let putRecords = await request({
+      method: 'PUT',
+      url: `https://api.cloudflare.com/client/v4/zones/${zone.trim()}/dns_records/${getRecords.result[0].content}`,
+      headers: headers,
+      body: { type: getRecords.result[0].type, name: domain, content: api.ip },
+      json: true
+    })
+    if (!putRecords.success) throw new Error(`Can't PUT record name '${domain}'`)
+    console.log(`[couldfare.com] Updated '${domain}' at ${moment().format('YYYY-MM-DD HH:mm:ss')} IP:'${api.ip}'.`)
+  }
 }
 
-console.log(`[couldfare.com] Watch and updated '${process.env.DOMAIN_NAME}'.`)
 ipAddrUpdate(process.env.DOMAIN_NAME).catch(Raven)
-
-let addrUpdateTime = '30 * * * *'
 let jobUpdated = new cron.CronJob({
-  cronTime: addrUpdateTime,
+  cronTime: '30 * * * *',
   onTick: () => { ipAddrUpdate(process.env.DOMAIN_NAME).catch(Raven) },
   start: true,
   timeZone: 'Asia/Bangkok'
 })
-console.log(`[couldfare.com] corntab(${addrUpdateTime}) ${jobUpdated.running ? 'started' : 'stoped'}.`)
+console.log(`[couldfare.com] Watch and updated '${process.env.DOMAIN_NAME}' ${jobUpdated.running ? 'started' : 'stoped'}.`)
